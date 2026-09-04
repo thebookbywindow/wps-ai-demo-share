f08b54ed1197ec17033fa61b2e1a792d4072e68b
thebookbywindow <2499039244@qq.com>
2026-09-04 16:24:54 +0800

    fix: serve template iframe as Pages asset

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
   server: {
@@ -14,18 +13,6 @@ export default defineConfig({
       name: 'sync-and-reload-templates',
       handleHotUpdate({ file, server }) {
         if (file.endsWith('templates-inner.html')) {
-          try {
-            // Also keep index.html base64 in sync for single-file portability
-            const inner = fs.readFileSync('templates-inner.html', 'utf8');
-            const index = fs.readFileSync('index.html', 'utf8');
-            const b64 = Buffer.from(inner, 'utf8').toString('base64');
-            const updatedIndex = index.replace(
-              /<script type="application\/octet-stream" id="templatesPayload">[\s\S]*?<\/script>/,
-              () => `<script type="application/octet-stream" id="templatesPayload">${b64}</script>`
-            );
-            fs.writeFileSync('index.html', updatedIndex, 'utf8');
-          } catch(e) {}
-
           // Notify browser client to reload
           server.ws.send({
             type: 'full-reload'
